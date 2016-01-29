917c16a20c86403e7fdeb6c1b9077a6e1de03830
Dave Humphrey <uespnet@gmail.com>
2016-01-28 19:44:28 -0500

    fix mobile site

--- wiki/config/InitializeSettings.php
+++ wiki/config/InitializeSettings.php
@@ -14,13 +14,22 @@ $uespIsMobile = false;
 
 $UESP_MOBILE_SERVERS = array(
 		'm.uesp.net',
+		'en.m.uesp.net',
+		'pt.m.uesp.net',
+		'www.m.uesp.net',
+		'content1.m.uesp.net',
+		'content2.m.uesp.net',
+		'content3.m.uesp.net',
 		'mobile.uesp.net',
 		'mobile1.uesp.net',
 		'mobile2.uesp.net',
 		'mobile3.uesp.net',
 );
 
-if (in_array($_SERVER['HTTP_HOST'], $UESP_MOBILE_SERVERS, TRUE)) $uespIsMobile = true;
+if (in_array($_SERVER['HTTP_HOST'], $UESP_MOBILE_SERVERS, TRUE)) 
+{
+	$uespIsMobile = true;
+}
 
 # Translation Projects and Languages
 
@@ -30,9 +39,23 @@ $uespLanguageSuffix = "";
 
 # TODO: More robust language detection
 $wgLanguageCode = "en";
-if ($_SERVER['HTTP_HOST'] == "pt.uesp.net") $wgLanguageCode = "pt";
+
+if ($_SERVER['HTTP_HOST'] == "pt.uesp.net" || $_SERVER['HTTP_HOST'] == "pt.m.uesp.net") 
+{
+	$wgLanguageCode = "pt";
+}
 
 if ($wgLanguageCode != "en")
 {
 	$uespLanguageSuffix = "_" . $wgLanguageCode;
+}
+
+# Set server according to environment and host name
+if ($uespIsDev)
+{
+	$wgServer = "//dev.uesp.net";
+}
+else 
+{
+	$wgServer = "//" . $wgLanguageCode . ".uesp.net";
 }
