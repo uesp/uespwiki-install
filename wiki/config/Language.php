299156038394d2984c77f2dbabb7e292c9de15a2
Dave Humphrey <uespnet@gmail.com>
2017-06-19 08:48:12 -0400

    config changes

---
+++ wiki/config/Language.php
@@ -0,0 +1,11 @@
+<?php
+# WARNING: This file is publically viewable on the web. Do not put private data here.
+#
+# This file contains language related configuration.
+# It is included by LocalSettings.php.
+#
+
+if ($wgLanguageCode != "en")
+{
+	$uespLanguageSuffix = "_" . $wgLanguageCode;
+}
